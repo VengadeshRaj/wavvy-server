b275200381f47ddb21142f717cb481d7668da9c6
vengadeshraj <vengadeshraj405@gmail.com>
2026-04-13 23:05:06 +0530

    server file initiate

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("i got hit..")
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(PORT, () => {
+  console.log(`server is up and running at ${PORT}..`);
+});
